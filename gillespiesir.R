5ca76a6dea465e9e4456d056de70b49bdb2e06e1
Eden Ng Tian Hwa <84511321+ethn1998@users.noreply.github.com>
2024-12-03 10:47:31 +0800

    reproductive number

--- gillespiesir.R
+++ gillespiesir.R
@@ -8,8 +8,9 @@ popsize <- 1000 #Population size
 t0 <- 0.0
 tmax <- 5.0
 
-beta <- 6.0/popsize #Infection rate, probability of 
+beta <- 6.0/popsize #Infection rate,
 gamma <- 2.0 #Recovery rate
+R_0 <- beta/gamma
 
 i0 <- 1.0*popsize/100 #Starting percentage of infected
 s0 <- popsize - i0
@@ -29,37 +30,42 @@ ivec <- c(i0)
 rvec <- c(r0)
 
 while(t<tmax){
-  p <- runif(1,0,1)
+  # Calculate reaction rates
   R_beta <- beta*s*i #Infection rate
   R_gamma <- gamma*i #Removal rate
   RTOT <- R_beta + R_gamma #Total rate
-  #print(sprintf("t:%.1e, pi:%.2e",t,R_beta/RTOT)) #Diagnostics
-  #Reaction
-  if(p < R_beta/RTOT){
-    s <- s-1
-    i <- i+1
-  } else{
-    i <- i-1
-    r <- r+1
+  if(RTOT>0){ #Only update while there's still something left to update
+    p <- runif(1,0,1)
+    # Determine Reaction
+    if(p < R_beta/RTOT){
+      s <- s-1
+      i <- i+1
+    } else{
+      i <- i-1
+      r <- r+1
+    }
+    #Update
+    t <- t + rexp(1,RTOT)
+    tvec <- append(tvec,t)
+    svec <- append(svec,s)
+    ivec <- append(ivec,i)
+    rvec <- append(rvec,r)
+  } else {
+    break
   }
-  #Update
-  t <- t + rexp(1,RTOT)
-  tvec <- append(tvec,t)
-  svec <- append(svec,s)
-  ivec <- append(ivec,i)
-  rvec <- append(rvec,r)
 }
 
+png("gillespieSIRtraj.png",height=2250 ,width=2250 ,units="px",res=300)
 plot(tvec,svec,xlim=c(0.0,tmax),ylim=c(0,popsize),type="l",xlab="Time",ylab="Number of individuals",
      main="SIR epidemic model",col="#d55e00")
 lines(tvec,ivec,col="#009e73")
 lines(tvec,rvec,col="#56b4e9")
-
+legend("right",legend=c("Susceptible","Infected","Removed"),col=c("#d55e00","#009e73","#56b4e9"),lty=c(1,1,1))
+dev.off()
 ### Tau leaping?
 
 
-
-### Euler method
+### Euler method for deterministic trajectory
 
 t <- 0.0
 s <- s0
@@ -85,16 +91,14 @@ while(t<tmax){
   rvec <- append(rvec,r)
 }
 
-lines(tvec,svec,xlim=c(0.0,tmax),ylim=c(0,popsize),type="l",xlab="Time",ylab="Number of individuals",
-     col="#d55e00",lty=2)
-lines(tvec,ivec,col="#009e73",lty=2)
-lines(tvec,rvec,col="#56b4e9",lty=2)
-#lines(tvec,svec+ivec+rvec,col="black") #sanity test
-legend("right",legend=c("Susceptible","Infected","Removed"),col=c("#d55e00","#009e73","#56b4e9"),lty=1)
+png("eulerSIRtraj.png",height=2250 ,width=2250 ,units="px",res=300)
+plot(tvec,svec,xlim=c(0.0,tmax),ylim=c(0,popsize),type="l",xlab="Time",ylab="Number of individuals",
+     main="SIR Epidemic model", sub=sprintf("R0 = %.2e",R_0),
+     col="#d55e00",lty=1,cex.main=2.0,cex.lab=1.5)
+lines(tvec,ivec,col="#009e73",lty=1)
+lines(tvec,rvec,col="#56b4e9",lty=1)
+#lines(tvec,svec+ivec+rvec,col="black") #sanity check
+legend("right",legend=c("Susceptible","Infected","Recovered"),col=c("#d55e00","#009e73","#56b4e9"),lty=c(1,1,1))
+dev.off()
 
 #Remark: numerical errors introduced breaks conservation of constant popsize. 
-
-### Runge-Kutta method
-
-
-
